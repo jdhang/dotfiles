295648184f286095240a4ef759b3a3b61159d262
Jason Hang <dev.jdhang@gmail.com>
2023-10-24 00:50:42 -0400

    Updated nvim and fish configs

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -18,6 +18,7 @@ local on_attach = function(_, bufnr)
   end
 
   nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+  nmap('<leader>r', vim.lsp.buf.references, '[R]eferences')
   nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
   nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
