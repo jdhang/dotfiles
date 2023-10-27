c05a02474f73be118f117f01f9e90b4a1236a96e
Jason Hang <jason.hang@clover.com>
2023-10-26 23:33:14 -0400

    Updated conflicting keymaps

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -5,7 +5,7 @@ vim.cmd.colorscheme('kanagawa')
 -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
 
 vim.keymap.set('n', '<leader>cr', '<cmd>colorscheme rose-pine<CR>',
-  { desc = 'set [c]olorscheme: rose-pine' })
+  { desc = 'set [c]olorscheme: [r]ose-pine' })
 vim.keymap.set('n', '<leader>ct', '<cmd>colorscheme tokyonight-night<CR>',
   { desc = 'set [c]olorscheme: [t]okyonight-night' })
 vim.keymap.set('n', '<leader>ck', '<cmd>colorscheme kanagawa<CR>',
