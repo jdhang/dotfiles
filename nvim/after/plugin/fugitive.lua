a90018a2bd16725b1128f2c1b16e3abbed065006
Jason Hang <jason.hang@clover.com>
2023-11-07 13:21:58 -0500

    Update configs

---
+++ nvim/after/plugin/fugitive.lua
@@ -0,0 +1,3 @@
+vim.keymap.set('n', '<leader>gs', '<cmd>Git<CR>', { desc = '[g]it - [s]tatus' })
+vim.keymap.set('n', '<leader>gb', '<cmd>Git blame<CR>', { desc = '[g]it - [b]lame' })
+vim.keymap.set('n', '<leader>gd', '<cmd>Git diff<CR>', { desc = '[g]it - [d]iff' })
