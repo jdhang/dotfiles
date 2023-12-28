40a738b419f2b4b6e8d537283697073b1dbd288b
Jason Hang <dev.jdhang@gmail.com>
2023-12-28 01:12:43 -0500

    transparent splits in nvim

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -4,6 +4,7 @@ vim.cmd.colorscheme('rose-pine')
 vim.opt.background = 'dark'
 
 vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
 
 vim.keymap.set('n', '<leader>cc', '<cmd>colorscheme catppuccin-mocha<CR>',
