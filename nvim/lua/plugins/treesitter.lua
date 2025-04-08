3b0212a5fc1c0dea1cc9d74f4bf4c61c66074c90
Jason Hang <jason.hang@clover.com>
2025-04-08 11:58:02 -0400

    Updates

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -16,27 +16,24 @@ return {
 
     config = function()
       require('nvim-treesitter.configs').setup {
-        ensure_installed = {
-          'c',
-          'css',
-          'go',
-          'html',
-          'javascript',
-          'jsdoc',
-          'json',
-          'lua',
-          'python',
-          'regex',
-          'tsx',
-          'typescript',
-          'vimdoc',
-        },
+        ensure_installed = { 'c', 'css', 'html', 'javascript', 'jsdoc', 'json', 'lua', 'luadoc', 'python', 'regex', 'tsx', 'typescript', 'vimdoc' },
 
         sync_install = false,
 
         auto_install = true,
 
-        highlight = { enable = true },
+        highlight = {
+          enable = true,
+          disable = function(lang, buf)
+            local max_filesize = 100 * 1024 -- 100 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+              return true
+            end
+          end,
+          additional_vim_regex_highlighting = false,
+        },
+
         indent = { enable = true },
       }
     end
