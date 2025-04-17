0e334c11854fa9aa3459f54626492ba284b64d86
Jason Hang <dev.jdhang@gmail.com>
2025-04-17 02:53:52 -0400

    Remove other surround and updated versions

--- nvim/lua/plugins/surround.lua
+++ nvim/lua/plugins/surround.lua
@@ -2,31 +2,11 @@ return {
   {
     "kylechui/nvim-surround",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    enabled = true,
     event = "VeryLazy",
     config = function()
       require("nvim-surround").setup({
         move_cursor = false,
       })
     end,
-  },
-
-  {
-    'echasnovski/mini.surround',
-    enabled = true,
-    version = false,
-    config = function()
-      require('mini.surround').setup({
-        mappings = {
-          add = "gsa",                      -- Add surrounding in Normal and Visual modes
-          delete = "gsd",                   -- Delete surrounding
-          find = "gsf",                     -- Find surrounding (to the right)
-          find_left = "gsF",                -- Find surrounding (to the left)
-          highlight = "gsh",                -- Highlight surrounding
-          replace = "gsr",                  -- Replace surrounding
-          update_n_lines = "gsn",           -- Update `n_lines`
-        },
-      })
-    end
   },
 }
